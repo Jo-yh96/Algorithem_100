9554ff237da59ba225812353d51af2b23c961c2c
Jo-yh96 <93243001+Jo-yh96@users.noreply.github.com>
2022-05-20 14:52:56 +0900

    [Bronze III] Title: 알람 시계, Time: 128 ms, Memory: 9332 KB -BaekjoonHub

---
+++ 백준/Bronze/2884. 알람 시계/알람 시계.js
@@ -0,0 +1,18 @@
+var fs =  require('fs')
+var input = fs.readFileSync('dev/stdin').toString().split(' ');
+
+let hour = Number(input[0]);  // Hour
+let minute = Number(input[1]);  // Minute
+
+minute -= 45;
+
+if (minute < 0) {
+    minute += 60;
+    hour--;
+
+    if (hour === -1) {
+        hour = 23;
+    }
+}
+
+console.log(hour + ' ' + minute);
