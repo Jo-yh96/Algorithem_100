4e38e08d00491bbdbe5e6756ddff60ec86b2429e
Jo-yh96 <93243001+Jo-yh96@users.noreply.github.com>
2022-06-29 14:59:11 +0900

    [Bronze II] Title: 숫자의 개수, Time: 120 ms, Memory: 9624 KB -BaekjoonHub

---
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.js
@@ -0,0 +1,7 @@
+let input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
+
+let num = String(input[0] * input[1] * input[2]);
+
+for (let i = 0; i <= 9; i++) { 
+   console.log(num.split(i + '').length - 1);    
+}
