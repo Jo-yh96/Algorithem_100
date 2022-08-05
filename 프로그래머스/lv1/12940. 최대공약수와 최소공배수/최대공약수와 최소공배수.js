268a312a4a650ad392ea2d5ed6d147befb7d98ce
Jo-yh96 <93243001+Jo-yh96@users.noreply.github.com>
2022-08-05 17:03:03 +0900

    [level 1] Title: 최대공약수와 최소공배수, Time: 0.07 ms, Memory: 29.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12940. 최대공약수와 최소공배수/최대공약수와 최소공배수.js
@@ -0,0 +1,10 @@
+function solution(n, m) {
+  const gcd = greatestCommonDivisor(n, m);
+  return [gcd, (n * m) / gcd];
+}
+
+const greatestCommonDivisor = (a, b) => {
+  if (b === 0) return a;
+  else return greatestCommonDivisor(b, a % b);
+};
+
